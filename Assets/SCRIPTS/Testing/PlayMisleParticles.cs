00b0f7b2aa19d28369eabd5f6d014b5782906812
Jakub <jakub.kicmal@gmail.com>
2017-11-10 15:50:53 +0100

    123

---
+++ Assets/SCRIPTS/Testing/PlayMisleParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMisleParticles : MonoBehaviour {
+
+	ParticleSystem part = new ParticleSystem();
+
+	// Use this for initialization
+	void Start () {
+		part = this.GetComponent<ParticleSystem> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			gameObject.GetComponent<SpriteRenderer> ().sprite = null;
+			part.Emit (10);
+			Invoke ("DestroyThisObject", (float)part.main.startLifetime.constant);
+		}
+	}
+	void DestroyThisObject() {
+		Destroy (this.gameObject);
+	}
+}
