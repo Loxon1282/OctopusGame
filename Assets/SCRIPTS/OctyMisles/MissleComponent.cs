7c986df4fadd2ab8fd68bd5b1063ef503ec0f13e
Jakub <jakub.kicmal@gmail.com>
2017-11-11 11:47:03 +0100

    asd

---
+++ Assets/SCRIPTS/OctyMisles/MissleComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleComponent : MonoBehaviour {
+
+	public float missileLifeTime;
+
+	public Vector3 customCrosshairPos;
+
+	void Awake() {
+		Invoke ("DestroyMissile", missileLifeTime);
+	}
+
+	void FixedUpdate () {
+		this.transform.position = Vector3.MoveTowards (this.transform.position,
+			customCrosshairPos, 0.04f);
+		if(this.transform.localScale.x > 0.3f)
+			this.transform.localScale -= new Vector3 (0.0005f, 0.0005f, 0);
+	}
+
+	void DestroyMissile() {
+		Destroy (GetComponent<CircleCollider2D>());
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = null;
+		this.gameObject.GetComponent<ParticleSystem> ().Emit (10);
+		Destroy (this.gameObject, this.gameObject.GetComponent<ParticleSystem> ().main.startLifetime.constant);
+	}
+}
---
+++ Assets/SCRIPTS/OctyMisles/MissleDispenser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleDispenser : MonoBehaviour {
+
+	public GameObject misslePrefab;
+	public GameObject crosshair;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			GameObject newMisle = Instantiate (misslePrefab);
+			newMisle.transform.position = this.transform.position;
+			newMisle.GetComponent<MissleComponent> ().customCrosshairPos = crosshair.transform.position;
+			newMisle = null;
+		}
+	}
+}
